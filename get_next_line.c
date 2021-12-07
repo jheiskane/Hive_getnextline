c4112ece448c9efbe48b1f4913cf9d482cf83d73
jheiskane <Juha@Juhas-Macbook.local>
2021-12-07 22:54:16 +0200

    Normed&Tested

--- get_next_line.c
+++ get_next_line.c
@@ -1,37 +1,115 @@
-#include "get_next_line.h"
-#include <stdio.h>
-#include <unistd.h>
-#include "libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheiskan <jheiskan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 22:36:26 by jheiskan          #+#    #+#             */
+/*   Updated: 2021/12/07 21:49:37 by jheiskan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
+
+t_line	*new_line(t_line *new, char buf[], t_var **var_s)
+{
+	int	i;
+
+	i = 0;
+	ft_bzero(buf, BUFF_SIZE + 1);
+	*var_s = (t_var *)malloc(sizeof(t_var));
+	(*var_s)->tmp = (char *)ft_memalloc(0);
+	(*var_s)->b_read = 1;
+	if (new == NULL)
+	{
+		new = (t_line *)malloc(sizeof(t_line));
+		new->s_data = ft_strnew(0);
+		new->l_saved = 0;
+	}
+	else if (new->s_data)
+	{
+		while (new->s_data[i] != '\0')
+		{
+			buf[i] = new->s_data[i];
+			i++;
+			(*var_s)->b_read = i;
+		}
+		ft_strdel(&(new->s_data));
+	}
+	new->l_len = -1;
+	return (new);
+}
+
+char	*ft_realloc(char *buf, char *str, int b_read)
+{
+	char	*tmp_ptr;
+
+	buf[b_read] = '\0';
+	tmp_ptr = str;
+	str = ft_strjoin(str, buf);
+	ft_strdel(&tmp_ptr);
+	return (str);
+}
+
+int	save_data(t_line **ptr, char *buf, t_var *s, char **line)
+{
+	int		saved_bytes;
+	t_line	*tmp;
+	int		l_len;
+
+	l_len = (*ptr)->l_len;
+	saved_bytes = s->b_read - l_len;
+	if (s->b_read > ++l_len)
+	{
+		tmp = (t_line *)ft_memalloc(sizeof(t_line));
+		tmp->s_data = ft_strsub((const char *)buf, l_len, saved_bytes);
+		tmp->s_data[saved_bytes - 1] = '\0';
+		tmp->l_saved = 1;
+		tmp->l_len = l_len - 1;
+		*ptr = tmp;
+	}
+	*line = ft_realloc(buf, s->tmp, l_len - 1);
+	return (1);
+}
+
+int	seek_char(char *str, int c, int len, int *l_saved)
+{
+	int	i;
+
+	i = 0;
+	*l_saved = 0;
+	while (i < len)
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 
 int	get_next_line(const int fd, char **line)
 {
-	//read
-	int		b_read;
+	t_var			*var_s;
+	char			buf[BUFF_SIZE + 1];
+	static t_line	*save;
 
-	b_read = 1;
-
-	*line = (char*)malloc(BUFF_SIZE);
-
-	while (b_read != 0)
+	save = new_line(save, &(*buf), &var_s);
+	while (var_s->b_read > 0 && save->l_len == -1 && fd >= 0 && line)
 	{
-		b_read = read(fd, &*line, BUFF_SIZE);
-		if (b_read != 0)
-			write(1, &*line, 1);
+		if (save->l_saved == 0)
+			var_s->b_read = read(fd, buf, BUFF_SIZE);
+		if (var_s->b_read > 0)
+		{
+			save->l_len = seek_char(buf, '\n', var_s->b_read, &(save->l_saved));
+			if (save->l_len == -1)
+				var_s->tmp = ft_realloc(buf, var_s->tmp, var_s->b_read);
+			else if (save->l_len >= 0)
+				return (save_data(&save, buf, var_s, line));
+		}
 	}
-	return (0);
-}
-
-int main()
-{
-	int fd;
-	char **line = NULL;
-
-	fd = open("testifilu", O_RDWR | O_CREAT);
-	ft_putstr_fd("Hello world!", fd);
-	printf("%d", get_next_line(fd, line));
-	printf("%s", *line);
-	close(fd);
-	free (*line);
-	return 0;
+	if (var_s->b_read < 0 || fd < 0 || !line)
+		return (-1);
+	*line = var_s->tmp;
+	return (!(buf[0] == '\0'));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,34 @@
-int get_next_line(const int fd, char **line);
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jheiskan <jheiskan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 21:47:28 by jheiskan          #+#    #+#             */
+/*   Updated: 2021/12/07 21:49:16 by jheiskan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <fcntl.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putchar_fd(char c, int fd);
+# include <stdlib.h>
+# include <fcntl.h>
+# include "./libft/libft.h"
+# define BUFF_SIZE 32
 
+typedef struct s_line
+{
+	char			*s_data;
+	int				l_len;
+	int				l_saved;
+}					t_line;
+typedef struct k_line
+{
+	char			*tmp;
+	int				b_read;
+}					t_var;
+int	get_next_line(const int fd, char **line);
 
-
-
-#define BUFF_SIZE 1
+#endif
